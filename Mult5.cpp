32afddd24c6978e52647820c66858460c545653a
Spaceza <137975622+Spaceza@users.noreply.github.com>
2024-11-11 07:50:53 -0300

    Add files via upload

---
+++ Mult5.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+  int a, b, soma;
+
+    printf("Digite o primeiro valor; ");
+    scanf("%d",&a);
+    printf("Digite o segundo valor; ");
+    scanf("%d",&b);
+   printf("%d x 4 = %d\n",a,a*4);
+   printf("%d x 3 = %d\n",b,b*3);
+  }
